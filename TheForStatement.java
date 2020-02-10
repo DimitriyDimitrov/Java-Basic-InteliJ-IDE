e7b29a423b03b4884fb2ef0f9c3feae37fb59e5a
DimitriyDimitrov <60811008+DimitriyDimitrov@users.noreply.github.com>
2020-02-10 11:24:41 +0200

    Add files via upload

---
+++ TheForStatement.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("10,000 at 1% interest = " + calculateInterest(10000.0, 1.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
+        System.out.println("10,000 at 6% interest = " + calculateInterest(10000.0, 6.0));
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println("10.000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+        }
+
+        int count = 0;
+        for (int i = 10; i < 15; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number!");
+                if (count == 5) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+    }
+
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+}
